7f4628e2f322e81336a99e7d2572ee17dff68b7f
olav <olavhd@gmail.com>
2018-11-06 21:04:53 +0100

    Update MyFirstPenguin/run.py

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -115,7 +115,7 @@ def rotateToEnemy(body):
             if body["you"]["direction"] == "right":
                 return ROTATE_RIGHT
             return ROTATE_LEFT
-    if body[enemies][0]["x"] < body["you"]["y"]:
+    if body[enemies][0]["y"] < body["you"]["y"]:
         if body["you"]["direction"] == "top":
             return ROTATE_LEFT
         return ROTATE_RIGHT
